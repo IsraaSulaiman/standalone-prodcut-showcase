542c0dc887e71880b924956fc294fe39f3d5c6c8
ranasobeid95 <ranasobeid95@gmail.com>
2023-07-19 23:11:59 +0300

    add header

--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -1,10 +1,22 @@
 import { HttpRequest, HttpHandlerFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { CookieManagementService } from '../services/cookie-management.service';
 
 export default function AppHttpInterceptor(
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) {
-  // add header to each request
-  // handle error
+  const cookieManagement = inject(CookieManagementService);
+
+  const TK = cookieManagement.getItem('TK');
+  if (TK)
+    return next(
+      req.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + TK,
+        },
+      })
+    );
+  else 
   return next(req);
 }
